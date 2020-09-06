d217de42da7701b648ce39c89d2ac27d422d262a
ZSabakh <zurabsabakhtarishvili@gmail.com>
2020-09-05 21:40:51 -0500

    Restructuring

--- App.js
+++ App.js
@@ -6,49 +6,59 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import { StatusBar } from "expo-status-bar";
 
 const Tab = createBottomTabNavigator();
 
+function Navigation() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "ios-home" : "ios-home";
+          } else if (route.name === "Main") {
+            iconName = focused ? "ios-musical-notes" : "ios-musical-note";
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "#E7700D",
+        inactiveTintColor: "gray",
+        activeBackgroundColor: "#212121",
+        style: {
+          height: 58,
+          //49 default
+          borderWidth: 0,
+          borderTopWidth: 1,
+          borderTopColor: "#151515",
+          backgroundColor: "#181818",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ tabBarVisible: false }}
+      />
+      <Tab.Screen name="Main" component={MainScreen} />
+    </Tab.Navigator>
+  );
+}
+
 function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === "Home") {
-              iconName = focused ? "ios-home" : "ios-home";
-            } else if (route.name === "Main") {
-              iconName = focused ? "ios-musical-notes" : "ios-musical-note";
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: "#E7700D",
-          inactiveTintColor: "gray",
-          activeBackgroundColor: "#212121",
-          style: {
-            height: 58,
-            //49 default
-            borderWidth: 0,
-            borderTopWidth: 1,
-            borderTopColor: "#151515",
-            backgroundColor: "#181818",
-          },
-        }}
-      >
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ tabBarVisible: false }}
-        />
-        <Tab.Screen name="Main" component={MainScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={{ flex: 1 }}>
+      <StatusBar backgroundColor="#00000090" />
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
+    </View>
   );
 }
 
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -9,7 +9,7 @@ import {
   TextInput,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { MaterialIcons } from "@expo/vector-icons";
+import Search from "../components/Search";
 
 function MainScreen() {
   return (
@@ -18,23 +18,9 @@ function MainScreen() {
       style={styles.gradient}
       start={[1.5, 0.4]}
     >
-      <StatusBar backgroundColor="#00000090" />
       <SafeAreaView style={styles.container}>
         {/* BEGINNING OF SEARCH BAR */}
-        <View style={styles.search}>
-          <View style={styles.searchBar}>
-            <MaterialIcons
-              style={styles.searchIcon}
-              name="search"
-              size={24}
-              color="black"
-            />
-            <TextInput style={styles.searchInput} />
-          </View>
-          <View style={styles.searchProfileCircle}>
-            <Text style={styles.searchProfileInitials}>ZS</Text>
-          </View>
-        </View>
+        <Search />
         {/* END OF SEARCHBAR */}
         <View style={styles.contentContainer}>
           <Text>Main screen!</Text>
@@ -51,52 +37,7 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
-  search: {
-    width: "100%",
-    height: 55,
-    backgroundColor: "#00000067",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  searchBar: {
-    // width: "60%",
-    // flexDirection: "row",
-    height: 40,
-    width: "60%",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#00000080",
-    marginHorizontal: 30,
-  },
-  searchIcon: {
-    padding: 5,
-  },
-  searchInput: {
-    // width: "100%",
-    // height: 33,
-    // backgroundColor: "#00000080",
-    // marginHorizontal: 30,
-    flex: 1,
-    paddingTop: 10,
-    paddingRight: 10,
-    paddingBottom: 10,
-    paddingLeft: 0,
-  },
-  searchProfileCircle: {
-    width: 44,
-    height: 44,
-    backgroundColor: "white",
-    borderRadius: 22,
-    marginRight: 30,
-    backgroundColor: "rgba(234, 166, 234, 0.84)",
-    justifyContent: "center",
-  },
-  searchProfileInitials: {
-    alignSelf: "center",
-    fontSize: 22,
-  },
+
   contentContainer: {
     width: "85%",
     marginTop: "2%",
